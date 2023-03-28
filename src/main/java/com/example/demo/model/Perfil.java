72c587383bd1a8664a84b742d494fb69741770d9
Sebastian Gomez <sbagomeznight@gmail.com>
2023-03-28 20:44:08 -0300

    agregado de @entity en la clase

--- src/main/java/com/example/demo/model/Perfil.java
+++ src/main/java/com/example/demo/model/Perfil.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
 public class Perfil
 {
     private int id;
