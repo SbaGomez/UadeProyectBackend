31585d2a7a35bce938aa4ba4072215290d33e047
Sebastian Gomez <sbagomeznight@gmail.com>
2023-03-28 21:10:03 -0300

    agregado anotacion @table

--- src/main/java/com/example/demo/model/Perfil.java
+++ src/main/java/com/example/demo/model/Perfil.java
@@ -1,6 +1,7 @@
 package com.example.demo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 @Entity
+@Table(name = "perfil")
 public class Perfil
 {
     private int id;
